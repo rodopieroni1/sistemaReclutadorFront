543b32178211e63c0d9d7223419ce63221d01e1f
Rodo <pieroni.rodrigo@gmail.com>
2025-03-19 09:40:22 -0300

    Develop Branch first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,7 @@ import { AuthComponent } from './service/auth.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `
-    <h1>¡Aplicación Angular con Firebase!</h1>
-    <app-auth></app-auth>
-  `,
+  template: ` <app-auth></app-auth> `,
   imports: [AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
