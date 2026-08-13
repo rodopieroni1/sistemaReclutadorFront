b47bd95b090a12157f1d28334c3534df2589f41b
Rodo <pieroni.rodrigo@gmail.com>
2026-08-13 16:24:49 -0300

    environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@
 
 export const environment = {
   production: true,
-  urlHost: 'http://backend:8080/',
-  urlApi: 'http://backend:8080/',
+  urlHost: '/api/',
+  urlApi: '/api/',
   userId: 1,
 };
