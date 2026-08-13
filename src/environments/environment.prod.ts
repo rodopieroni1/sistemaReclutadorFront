f88976003fea61856efdb4012c669baa245d9f7e
Rodo <pieroni.rodrigo@gmail.com>
2026-08-13 16:58:04 -0300

    Configurar entorno de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,22 @@
-/*"configurations": {
-  "production": {
-    "fileReplacements": [
-      {
-        "replace": "src/environments/environment.ts",
-        "with": "src/environments/environment.prod.ts"
-      }
-    ],
-  }
-}*/
-
 export const environment = {
   production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyBRF3EEN5XHk1kaMnOQi5e9COzv3muyqSA',
+    authDomain: 'proyectoreclutador-ee62f.firebaseapp.com',
+    projectId: 'proyectoreclutador-ee62f',
+    storageBucket: 'proyectoreclutador-ee62f.firebasestorage.app',
+    messagingSenderId: '365573390664',
+    appId: '1:365573390664:web:f7c5e2408a86702225337d',
+    measurementId: 'G-D343425N81',
+  },
+
+  local: {
+    urlHost: '/api/',
+    urlApi: '/api/',
+    userId: 1,
+  },
+
   urlHost: '/api/',
   urlApi: '/api/',
   userId: 1,
